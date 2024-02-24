95761bd58d3fdad4abde2f30c9ebd5e61aee345f
HennrryCanahui <108434290+HennrryCanahui@users.noreply.github.com>
2024-02-24 10:46:25 -0600

    Update corto.cpp

--- corto.cpp
+++ corto.cpp
@@ -12,7 +12,8 @@ int main() {
         cout << N_natural << " al cuadrado es: " << cuadrado << "\n";
         contador += 1;
     }
-
+    // ejercicio 2
+    
         int numero, i, resto;
     cout << "Este programa muestra los divisores de un numero." << endl;
     cout << "ingrese un numero: ";
@@ -23,6 +24,24 @@ int main() {
         if (resto == 0){
             cout << i << " ";
         }
+    }
+    // ejercicio 3
+    int calificacion;
+    cout << "Ingrese una calificacion numerica (0-100): ";
+    cin >> calificacion;
+
+    if (calificacion >= 90 && calificacion <= 100) {
+        cout << "La calificacion es: A" << endl;
+    } else if (calificacion >= 80 && calificacion <= 89) {
+        cout << "La calificacion es: B" << endl;
+    } else if (calificacion >= 70 && calificacion <= 79) {
+        cout << "La calificacion es: C" << endl;
+    } else if (calificacion >= 60 && calificacion <= 69) {
+        cout << "La calificacion es: D" << endl;
+    } else if (calificacion >= 0 && calificacion <= 59) {
+        cout << "La calificacion es: F" << endl;
+    } else {
+        cout << "Calificacion invalida. Por favor, ingrese un numero entre 0 y 100." << endl;
     }
     return 0;
 }
